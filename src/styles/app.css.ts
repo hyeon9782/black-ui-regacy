2edf32cdca7ab5650ac0d52423333ea0f0f7f8f6
hyeon9782 <hyeon9782@gmail.com>
2023-12-08 17:29:47 +0900

    feat: vanilla extract 의존성 설치 및 기본 기본 코드 작성

---
+++ src/styles/app.css.ts
@@ -0,0 +1,7 @@
+import { globalStyle } from "@vanilla-extract/css";
+
+globalStyle("body", {
+  margin: 0,
+  height: "100%",
+  backgroundColor: "red",
+});
---
+++ src/styles/common.css.ts
@@ -0,0 +1,7 @@
+import { style } from "@vanilla-extract/css";
+
+export const container = style({
+  backgroundColor: "black",
+  width: "100px",
+  height: "100px",
+});
