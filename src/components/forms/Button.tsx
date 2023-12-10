94972be8b50e5beeaea09062e7196b40e1886967
hyeon9782 <hyeon9782@gmail.com>
2023-12-10 12:02:13 +0900

    feat: Button 컴포넌트 구현 및 Storybook 코드 추가

--- src/components/forms/Button.tsx
+++ src/components/forms/Button.tsx
@@ -1,5 +1,33 @@
-const Button = () => {
-  return <button>Button</button>;
+import { basicButton } from "../../styles/button.css";
+
+type ButtonColor = "black" | "blue" | "red";
+
+type ButtonSize = "sm" | "md" | "lg";
+
+type ButtonVariant = "solid" | "outline";
+
+type ButtonProps = {
+  label: string;
+  color?: ButtonColor;
+  size?: ButtonSize;
+  variant?: ButtonVariant;
+  onClick: () => void;
 };
+
+const Button = ({
+  label,
+  color = "black",
+  size = "md",
+  variant = "solid",
+  onClick,
+}: ButtonProps) => {
+  return (
+    <button onClick={onClick} className={basicButton}>
+      {label}
+    </button>
+  );
+};
+
+function selectedStyles(color, size, variant) {}
 
 export default Button;
---
+++ src/styles/button.css.ts
@@ -0,0 +1,6 @@
+import { style } from "@vanilla-extract/css";
+
+export const basicButton = style({
+  backgroundColor: "white",
+  borderStyle: "none",
+});
