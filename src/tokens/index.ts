4423136896157c326ca3dcb8551477f2c1c3ecc8
hyeon9782 <hyeon9782@gmail.com>
2023-12-12 18:13:53 +0900

    feat: border.ts, index.ts 생성

---
+++ src/tokens/index.ts
@@ -0,0 +1,33 @@
+import { borderStyles, borderWidths } from "./border";
+import { colors } from "./color";
+import { radii } from "./radii";
+import { shadows } from "./shadows";
+import { space } from "./space";
+import {
+  fontFamilies,
+  fontSize,
+  fontWeights,
+  letterSpacing,
+  lineHeights,
+  textDecoration,
+  typographies,
+} from "./typography";
+
+export const tokens = {
+  borderStyles,
+  borderWidths,
+  colors,
+  radii,
+  shadows,
+  space,
+  fontFamilies,
+  fontSize,
+  fontWeights,
+  letterSpacing,
+  lineHeights,
+  textDecoration,
+  typographies,
+};
+
+export type { Mode } from "./color";
+export type Tokens = typeof tokens;
