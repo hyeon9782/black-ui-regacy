fd4e6233e9a250d72e88d5c15fdd0d69b660ed29
hyeon9782 <hyeon9782@gmail.com>
2023-12-15 21:15:19 +0900

    feat: responsiveStyles.ts 생성

---
+++ src/styles/responsiveStyle.ts
@@ -0,0 +1,47 @@
+import { StyleRule } from "@vanilla-extract/css";
+
+import { Breakpoint, breakpoints } from "./breakpoints";
+
+type CSSProps = Omit<StyleRule, "@media" | "@supports">;
+
+const makeMediaQuery = (breakpoint: Breakpoint) => (styles?: CSSProps) =>
+  !styles || Object.keys(styles).length === 0
+    ? {}
+    : {
+        [`screen and (min-width: ${breakpoints[breakpoint]}px)`]: styles,
+      };
+
+const mediaQuery = {
+  mobile: makeMediaQuery("mobile"),
+  tablet: makeMediaQuery("tablet"),
+  desktop: makeMediaQuery("desktop"),
+};
+
+type ResponsiveStyle = {
+  wide?: CSSProps;
+  mobile?: CSSProps;
+  tablet?: CSSProps;
+  desktop?: CSSProps;
+};
+
+export const responsiveStyle = ({
+  wide,
+  mobile,
+  tablet,
+  desktop,
+}: ResponsiveStyle): StyleRule => {
+  const { "@media": _, ...wideStyle } = (wide ?? {}) as any;
+
+  return {
+    ...wideStyle,
+    ...(mobile || tablet || desktop
+      ? {
+          "@media": {
+            ...mediaQuery.mobile(mobile ?? {}),
+            ...mediaQuery.tablet(tablet ?? {}),
+            ...mediaQuery.desktop(desktop ?? {}),
+          },
+        }
+      : {}),
+  };
+};
